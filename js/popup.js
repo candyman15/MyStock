77b21e409d9a1852d8e1f0e496dd38949d11b9fa
박동혁 <dhpark@ysmcorp.com>
2016-05-10 17:33:03 +0900

    1. 리스트 추가

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,20 @@
+var interestCompanyName = [];
+var interestCompanyCode = [];
+var STORAGE_KEY_COMPANY_NAME = "company_name";
+var STORAGE_KEY_COMPANY_CODE = "company_code";
+
 $(document).ready(function() {
     //searchCompanyCode();
     attachEvent();
+
+    //localStorage.setItem("save","");
+    //localStorage.clear();
+    interestCompanyName.push(localStorage.getItem(STORAGE_KEY_COMPANY_NAME));
+    interestCompanyCode.push(localStorage.getItem(STORAGE_KEY_COMPANY_CODE));
+    
+    console.info(localStorage.getItem(STORAGE_KEY_COMPANY_NAME));
+    console.info(localStorage.getItem(STORAGE_KEY_COMPANY_CODE));
+    makeInterestList();
 });
 
 function attachEvent(){
@@ -16,6 +30,7 @@ function attachEvent(){
 
 function searchCompanyCode(name){
     $("#body").html("");
+
     var requestUrl = "http://ac.finance.naver.com:11002/ac?_callback=&q="+name+"&q_enc=euc-kr&t_koreng=1&st=111&r_lt=111";
     console.info("requestUrl:"+requestUrl);
     var xhr = new XMLHttpRequest();
@@ -31,14 +46,13 @@ function searchCompanyCode(name){
 
                 var items = results.items[0][i];
 
-                console.info("items :" + items);
-
                 var innerHtml = "<div class='col-sm-3 col-md-4'>"+
-                    "<div class='thumbnail' style='height:70px;'>"+
+                    "<div class='thumbnail' style='height:100px;'>"+
                     "<input type='hidden'  class='link'>" +
                     "<div class='caption'>" +
-                    "<h6 style='cursor: pointer; !important;'>"+items[0]+"</h6>" +
                     "<p style='cursor: pointer; !important;'>"+items[1]+"</p>" +
+                    "<p style='cursor: pointer; !important;'>"+items[0]+"</p>" +
+                    "<button onclick=javascript:addInterest('"+items+"')>추가</button>" +
                     "</div>" +
                     "</div>" +
                     "</div>";
@@ -92,6 +106,45 @@ function searchCompanyCode(name){
     */
 }
 
+function makeInterestList(){
+    var code = JSON.parse(localStorage.getItem(STORAGE_KEY_COMPANY_CODE));
+    var name = JSON.parse(localStorage.getItem(STORAGE_KEY_COMPANY_NAME));
+
+    for(var i in code){
+
+        var innerHtml = "<div class='col-sm-3 col-md-4'>"+
+            "<div class='thumbnail' style='height:100px;'>"+
+            "<input type='hidden'  class='link'>" +
+            "<div class='caption'>" +
+            "<p style='cursor: pointer; !important;'>"+code[i]+"</p>" +
+            "<p style='cursor: pointer; !important;'>"+name[i]+"</p>" +
+            "</div>" +
+            "</div>" +
+            "</div>";
+
+        $("#body").append(innerHtml);
+    }
+}
+
+function addInterest(items){
+    console.info("items :" + items);
+    //var company = [];
+    var value = items.split(',');
+    //for(var i=0; i < value.length; i++){
+    //    company.push(value[i]);
+    //}
+
+    interestCompanyCode.push(value[0]);
+    interestCompanyName.push(value[1]);
+
+    console.info("value[0] :" + value[0]);
+    console.info("value[1] :" + value[1]);
+
+    localStorage.setItem(STORAGE_KEY_COMPANY_CODE, JSON.stringify(interestCompanyCode));
+    localStorage.setItem(STORAGE_KEY_COMPANY_NAME, JSON.stringify(interestCompanyName));
+
+    //
+}
 
 
 function moveOneshop(tab) {
